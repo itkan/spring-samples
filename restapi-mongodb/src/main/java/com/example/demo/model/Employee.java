6b1532bd4fff20398dc6b77298ac3ecb9131fe41
Ankit Kumar Gupta <itkan@users.noreply.github.com>
2019-01-13 20:45:37 +0530

    remove unused jackson dependency

--- restapi-mongodb/src/main/java/com/example/demo/model/Employee.java
+++ restapi-mongodb/src/main/java/com/example/demo/model/Employee.java
@@ -3,8 +3,6 @@ package com.example.demo.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NonNull;
@@ -14,16 +12,9 @@ import lombok.Setter;
 @Getter @Setter
 @Document(collection = "employee")
 public class Employee {
-	@JsonProperty
 	@Id 
 	private @NonNull String id;
-
-	@JsonProperty
 	private @NonNull String firstName;
-
-	@JsonProperty
 	private @NonNull String lastName;
-
-	@JsonProperty
 	private @NonNull String email;
 }
