be13925ef549398f76f48c874c2ed3ff17528c30
Ankit Kumar Gupta <itkan@users.noreply.github.com>
2019-01-18 18:22:32 +0530

    Added cassandra sample (#5)

---
+++ restapi-cassandra/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+/*
+ * http://www.technicalkeeda.com/spring-tutorials/spring-4-mongodb-repository-example
+ * https://www.mongodb.org/
+ * https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/
+ * https://www.baeldung.com/spring-data-mongodb-tutorial
+ * https://docs.mongodb.com/manual/mongo/
+ */
---
+++ restapi-cassandra/src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.model.Employee;
+
+public interface EmployeeRepository extends CrudRepository<Employee, String> {
+}
